5b0346b723c39ab6bc8382ef6091ac6fe966fe20
E25dgar <60941739+E25dgar@users.noreply.github.com>
2020-03-09 14:17:49 -0500

    Add files via upload

---
+++ Operacionespares.java
@@ -0,0 +1,41 @@
+
+package operacionespares;
+
+public class Operacionespares {
+
+   
+    public static void main(String[] args) {
+       
+        int =0 , lim=20, contP=0, contI=0, acumP=0, acumI=0;
+        
+        
+        for (i=1 ; i<=lim; i++)<{
+        System.out.println(i);
+        
+        if (i % 2 == 0);
+        contP=conP+1;
+        
+        acumP=acumP+i;
+        
+        
+       }else<{
+        
+        contI ++;
+        
+        acumI=acumI+i;
+          
+    }
+        
+     System.out.println("Total de pares :"+contP);
+     System.out.println("suma  de pares :"+acumP);
+     System.out.println("Total de impares :"+contI);
+     System.out.println("suma  de pares :"+acumI);
+     System.out.println("VALOR FINAL DE I :"+i);
+     System.out.println("VALOR FINAL DE LIM :"+lim);
+        
+        
+        
+        
+    }
+    
+}
