ed22e0e233ac6dd1416e8668079091c02f667573
E25dgar <60941739+E25dgar@users.noreply.github.com>
2020-03-11 14:40:07 -0500

    Update sumapar.java

--- sumapar.java
+++ sumapar.java
@@ -9,21 +9,18 @@ public class sumapar {
       
 
     public static void main(String[] args) {
-        
-        sumapar cn = new sumapar();
-        cn.mostrar();
       
-     
-    }
-
-    private void mostrar() {
         Scanner leer=new Scanner(System.in);
         int numero;
         int mayor=0;
-         int menor=1000000;
+        int menor=1000000;
+        int contP=0;
+        int acumP=0;
+        int contI=0;
+        int acumI=0;
+        int i=0
         
-        
-        for (int i=1; i<=4; i++){
+        for (i=1; i<=4; i++){
             System.out.println("ingrese un numero:");
             
             numero=leer.nextInt();
@@ -37,18 +34,32 @@ public class sumapar {
                  
                  
              }
+              if (i % 2 == 0){
+                 contP=contP+1;
+        
+                acumP=acumP+i;
+        
+        
+              }else{
+        
+                 contI ++;
+        
+                acumI=acumI+i;
+          
+               }
             
             
         }
         System.err.println("numero mayor :"+mayor);
         System.out.println("numero menor:"+menor);
-        
+        System.out.println("suma  de pares :"+acumP);
+        System.out.println("suma  de impares :"+acumI);
         
         
     }
     
     
     
-    
+   }  
     
 }
