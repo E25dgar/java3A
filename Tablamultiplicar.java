d46f53c6a64645e754f4c5fb085cde3713a4f577
E25dgar <60941739+E25dgar@users.noreply.github.com>
2020-03-11 11:49:04 -0500

    Add files via upload

---
+++ Tablamultiplicar.java
@@ -0,0 +1,23 @@
+
+
+package tablamultiplicar;
+
+
+public class Tablamultiplicar {
+
+   
+    public static void main(String[] args) {
+       
+      for (int i=1; i<=10; i++){
+          
+          for (int j=0; j<=10; j++){
+              System.out.println(i + "*" + j +"=" +i*j);
+              
+              
+          }
+          System.out.println();
+      }  
+        
+    }
+    
+}
